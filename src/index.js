996b14d95c98bf63ce7e9c74fe5b2ac5211877a8
Darshana Pathak <sonal.darshana@gmail.com>
2018-04-26 00:41:07 +1000

    modified functional component to class component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAxjFJrP9jm7VX5GQcQ1avrhMQM16XSdtQ' ;
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+
+class App extends Component {
+	constructor(props){
+		super(props)
+
+		this.state = {videos: []}
+		YTSearch({ key:API_KEY, term:'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+
+	}
+	
 }
 
 ReactDOM.render(<App />, document.getElementById("container"));
